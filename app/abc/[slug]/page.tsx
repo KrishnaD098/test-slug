d3fee3f95e62cc655d1c98e7b6f2a01fa289356d
Krishna <dhootkrishna123@gmail.com>
2025-04-21 02:36:34 +0530

    handled page params as promise

--- app/abc/[slug]/page.tsx
+++ app/abc/[slug]/page.tsx
@@ -3,8 +3,10 @@ import path from 'path';
 
 const db = new FileDBAdapter(path.join(process.cwd(), 'blog-data/'));
 
-export default async function BlogPage({ params }: { params: { slug: string } }) {
-    const blog = await db.blogs.findOne({ slug: params.slug });
+export default async function BlogPage({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+
+    const blog = await db.blogs.findOne({ slug: slug });
 
     return (
         <div className="p-6 max-w-2xl mx-auto">
